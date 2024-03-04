ce4e857830de4fb94775ac5009c86eda273f78eb
Manogya <bat16692@gmail.com>
2024-03-04 16:06:28 +0545

    A simple for loop program

---
+++ basic-prog/loop.c
@@ -0,0 +1,23 @@
+//program for for loop 
+
+
+#include <stdio.h>
+
+void main() {
+
+int i=0;  //initializing i
+for(; ; ){ 
+
+	if (i==5) //condition which is to be met for termination of program
+	{
+    	
+		break; 
+	}
+	
+	printf("%d\n",i);
+	
+	i++; //increament
+
+	}	
+
+}
